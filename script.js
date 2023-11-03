0d0fb044d630b8dce93bd3004bf1250d527f7f9f
lohit-19 <lohitvenkatasai2004@gmail.com>
2023-11-03 14:16:28 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const images = ["images/1535.jpg","images/1535 (1).jpg","images/1535 (2).jpg","images/1535 (3).jpg","images/1535 (4).jpg","images/1535 (5).jpg","images/1535 (6).jpg","images/1535 (7).jpg"]
+
+const cells={
+    '1' : "",
+    '2' : "",
+    '3' : "",
+    '4' : "",
+    '5' : "",
+    '6' : "",
+    '7' : "",
+    '8' : "",
+    '9' : "",
+    '10' : "",
+    '11' : "",
+    '12' : "",
+    '13' : "",
+    '14' : "",
+    '15' : "",
+    '16' : "",
+};
+//console.log();
+function assign_random(){
+    let imgs = [...images];
+    let idx1 = Math.trunc(Math.random()*8;
+    if(imgs[idx1] isin )
+    
+}
+assign_random();
